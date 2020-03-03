72e2927141f68dde45dfdbf162c8915bd9125dcb
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-03-02 20:01:58 -0800

    Add to DAFx paper

--- hydroflask_water_mode_analysis.py
+++ hydroflask_water_mode_analysis.py
@@ -10,17 +10,21 @@ x = np.linspace(0, 1, 7)
 x_long = np.linspace(0, 1, 100)
 
 # %%
-plt.figure()
-mode_freqs = np.abs(mode_data[:,0])
-plt.plot(x, mode_freqs)
+# plt.figure()
+# mode_freqs = np.abs(mode_data[:,0])
+# plt.plot(x, mode_freqs)
 
-z_freq = np.polyfit(x, mode_freqs, 3)
-p_freq = np.poly1d(z_freq)
-plt.plot(x_long, p_freq(x_long))
-print('Freq function:')
-print(p_freq)
+# z_freq = np.polyfit(x, mode_freqs, 3)
+# p_freq = np.poly1d(z_freq)
+# plt.plot(x_long, p_freq(x_long))
+# print('Freq function:')
+# print(p_freq)
 
-plt.title('First mode frequency vs water amount')
+# plt.title('First mode frequency vs water amount')
+# plt.ylabel('Frequency [Hz]')
+# plt.xlabel('Water as fraction of bottle volume')
+# plt.xticks(x, ['0', '1/32', '1/16', '1/8', '1/4', '1/2', '1'])
+# plt.legend(['Measured', 'Modelled'])
 
 # %%
 plt.figure()
@@ -29,15 +33,18 @@ for i in range(2):
     plt.plot(x, mode_damps, label='Mode #{}'.format(i+1))
 
 mode_damps = np.mean(np.abs(mode_data[:,40:42]), axis=1)
-plt.plot(x, mode_damps, label='Avg')
+# plt.plot(x, mode_damps, label='Avg')
 
 z_damp = np.polyfit(x, mode_damps, 4)
 p_damp = np.poly1d(z_damp)
-plt.plot(x_long, p_damp(x_long))
+plt.plot(x_long, p_damp(x_long), label='Model')
 print('Damp function:')
 print(p_damp)
 
 plt.legend()
 plt.title('First 2 modes damping vs water amount')
+plt.ylabel('Tau [gain/sample]')
+plt.xlabel('Water as fraction of bottle volume')
+plt.xticks(x, ['0', '1/32', '1/16', '1/8', '1/4', '1/2', '1'])
 # %%
 plt.show()
