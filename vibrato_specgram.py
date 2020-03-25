d7733f91025c53d3580bfc88893af52295730244
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-03-25 14:17:11 -0700

    Spectrogram of waterbottle with swinging vibrato

---
+++ vibrato_specgram.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy.signal as signal
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+
+fs, x = wavfile.read('Audio/Hydroflask/third_full_vibrato.wav')
+
+x = x[:,0] / 2**15
+
+f, t, Sxx = signal.spectrogram(x, fs, nperseg=2048, nfft=4096*8)
+Sxx /= np.max(np.abs(Sxx))
+plt.pcolormesh(t, f, 20*np.log10(np.abs(Sxx)), cmap='inferno', vmin=-120)
+plt.colorbar()
+plt.yscale('log')
+plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter())
+plt.ylim(60, 20000)
+plt.xlim(0, 4)
+
+plt.xlabel('Time [seconds]')
+plt.ylabel('Frequency [Hz]')
+plt.title('Synthesized Waterbottle with Swinging Vibrato')
+plt.savefig('Figures/Specgrams/Vibrato.png')
+
+plt.show()
